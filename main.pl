7b3ae26b8a5e66e65acc787875a4015c2b9d1b50
Yashar <ya.atajan@gmail.com>
2018-02-07 16:27:18 -0500

    minor Beautification.

--- main.pl
+++ main.pl
@@ -6,13 +6,16 @@ use feature 'say';
 use feature 'switch';
 
 #program to find doi from a text file
-print "This program will find doi string from a text file.\n";
+say "This program will find doi string from a text file.\n\n";
 
 #code
 my $string = "Abstract [This corrects the article DOI: 10.1371/journal.pone.0163238.].
 Erratum for Consensus Analysis of Whole Transcriptome Profiles from Two Breast Cancer
 Patient Cohorts Reveals Long Non-Coding RNAs Associated with Intrinsic
-Subtype and the Tumour Microenvironment. [PLoS One. 2016]";
+Subtype and the Tumour Microenvironment. [PLoS One. 2016]\n";
+
+say $string;
+
 while ( $string =~ /(\w*[aeiou]{2}\w*)/g ) {
-  say "$1"
+  say "$1";
 }
