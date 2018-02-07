ed857473082f384cdaeefe402cfb9cd6910f8bba
Yashar <ya.atajan@gmail.com>
2018-02-07 13:41:03 -0500

    Initial commit

---
+++ main.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+use feature 'switch';
+
+#info
+my $name = 'Yaxiaer Atajiang';
+
+#program to find doi from a text file
+print "This program will find doi string from a text file.\n";
